fcae75aa0b8a9c3fd77ff78e0e2773b064497f97
Saurav Paul <35382323+Saurav-Paul@users.noreply.github.com>
2019-02-27 04:05:44 +0600

    Create STRHH - Half of the half.cpp

---
+++ STRHH - Half of the half.cpp
@@ -0,0 +1,66 @@
+#include<bits/stdc++.h>
+#define endl '\n'
+#define _time_ clock_t tStart = clock();
+#define _show_ printf("\nTime taken: %.6fs\n", (double)(clock() - tStart)/CLOCKS_PER_SEC);
+#define ll long long int
+#define loop(a,b)             for(int i=a;i<=b;++i)
+#define pb                    push_back
+#define F                     first
+#define S                     second
+#define mp                    make_pair
+#define clr(x)                x.clear()
+#define MOD                   1000000007
+#define itoc(c)               ((char)(((int)'0')+c))
+#define vi                    vector<int>
+#define all(p)                p.begin(),p.end()
+#define mid(s,e)              (s+(e-s)/2)
+#define sv()                  ll t,n; scanf("%lld",&t);n=t; while(t--)
+#define tcase()               ll t,n; cin>>t;n=t; while(t--)
+#define iscn(num)             scanf("%d",&num);
+using namespace std;
+bool file=0,rt=1;
+void FAST_IO();
+////////////////////////
+
+int main()
+{
+    _time_ 
+    FAST_IO();
+    ////////////////////////
+    ll tcase,sz;
+    cin>>tcase;
+    while(tcase--){
+        string s;
+        cin>>s;
+        sz=s.size();
+        sz/=2;
+        int i=2;
+        cout<<s[0];
+        while(sz>i){
+            cout<<s[i];
+            i+=2;
+        }
+        cout<<endl;
+    }
+    
+    
+    ////////////////////////
+    if(file && rt){ _show_ } 
+
+    return 0;
+   }
+
+void FAST_IO()
+{
+    ios_base::sync_with_stdio(0);
+    //cin.tie(0);
+    //cout.tie(0);
+    //cout.setf(ios::fixed);
+    //cout.precision(2);
+    if(file){
+    #ifndef _offline
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+    }
+}
