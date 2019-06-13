74c5cb51e9242b6cadb8aa7d44d30e989f49a1a7
Saurav Paul <35382323+Saurav-Paul@users.noreply.github.com>
2019-06-13 20:40:07 +0600

    Brute_force

---
+++ Best Time to Buy and Sell Stock.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int nax = prices.size() ;
+        int ans = 0;
+        
+        for(int i = 0 ; i < nax ; i++){
+           for(int j = i; j< nax ; j++){
+               if(prices[i] > prices[j])
+                    break ;
+               ans = max(ans, prices[j]-prices[i]);
+           }
+            
+        }
+        return ans ;
+        
+    }
+};
