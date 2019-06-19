b1538b94aca32cd191dc1f5b7603d67ae8c52822
Saurav Paul <35382323+Saurav-Paul@users.noreply.github.com>
2019-06-19 12:41:41 +0600

    Implementation

---
+++ leetCode-Fibonacci Number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int fib(int N) {
+        if(N==0)
+            return 0;
+        int a = 0, b = 1;
+        for(int i =2  ; i <=  N ; i++){
+            int c = a + b;
+            a = b;
+            b = c;
+        }
+        return b;
+    }
+};
