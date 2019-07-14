b004e0f7ee14c627485c906593a338c96ac9408d
Saurav Paul <35382323+Saurav-Paul@users.noreply.github.com>
2019-07-14 23:02:09 +0600

    Math

---
+++ A - Remove a Progression.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve(){
+	
+	int n, k;
+	scanf("%d%d",&n,&k);
+	printf("%d\n",2*k);
+	
+}
+
+int main()
+{
+	int n;
+	scanf("%d",&n);
+	while(n--)
+		solve();
+	
+    return 0;
+}
