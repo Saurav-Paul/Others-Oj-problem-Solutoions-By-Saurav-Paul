acc39c51784f880cef125fe80d1c6b94e7cd39fe
Saurav Paul <35382323+Saurav-Paul@users.noreply.github.com>
2019-07-25 16:46:06 +0600

    Stdin, stdout

---
+++ Java Stdin and Stdout I.cpp
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int a, b, c;
+        
+        Scanner input = new Scanner(System.in);
+        a = input.nextInt();
+        b = input.nextInt();
+        c = input.nextInt();
+        
+        System.out.println(a+"\n"+b+"\n"+c);
+        
+        input.close();
+    }
+}
+
