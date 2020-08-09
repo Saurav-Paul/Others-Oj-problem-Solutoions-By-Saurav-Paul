0646146a9c7da21e0bacffa1fb03c9e8c3bc5c3a
Saurav Paul <35382323+Saurav-Paul@users.noreply.github.com>
2020-08-09 11:17:38 +0600

    Create Leetcode - Move Zeroes .cpp

---
+++ Leetcode - Move Zeroes .cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int pos = 0 ;
+        for(int i = 0 ; i < nums.size() ; i++){
+            if(nums[i]) swap(nums[i],nums[pos++]) ;
+        }
+    }
+};
